efc9e5f6bcd8ada202a1a11dcc79fa4d7a10a80e
najmaA <najmaamin10@gmai.com>
2019-12-02 12:42:12 +0300

    form mudule

--- src/app/quote/form/form.component.ts
+++ src/app/quote/form/form.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote(0,"","",new Date());
   constructor() { }
 
   ngOnInit() {
