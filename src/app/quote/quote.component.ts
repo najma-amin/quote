c7e1d87f07dc507cdd67365905ccfdfa5f7a5f03
najmaA <najmaamin10@gmai.com>
2019-12-02 16:35:30 +0300

    boostrap col and row added

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,11 +10,11 @@ export class QuoteComponent implements OnInit {
 
   quote:Quote[] = [
     new Quote (1, 'posted on', 'Programs must be written for people to read, and only incidentally for machines to execute. ',new Date(2012,3,14)),
-    new Quote(2,'posted on','I have to buy cookies for the parrot',new Date(2017,3,14)),
-    new Quote(3,'posted on','Diana has her birthday coming up soon',new Date(2016,3,14)),
-    new Quote(4,'posted on','Pupper likes expensive snacks',new Date(2018,3,14)),
-    new Quote(5,'posted on','Damn Math',new Date(2019,3,14)),
-    new Quote(6,'posted on','Cause I am an evil overlord',new Date(2013,3,14)),
+    new Quote(2,'posted on','The fool speaks, the wise man listens',new Date(2017,3,14)),
+    new Quote(3,'posted on','For every minute you are angry you lose sixty seconds of happiness ',new Date(2016,3,14)),
+    new Quote(4,'posted on','Wisdom does not come overnight.',new Date(2018,3,14)),
+    new Quote(5,'posted on','I belong to everybody and I belong to nobody',new Date(2019,3,14)),
+    new Quote(6,'posted on','always stay',new Date(2013,3,14)),
   ];
   toggleDetails(index){
     this.quote[index].showDescription = !this.quote[index].showDescription;
