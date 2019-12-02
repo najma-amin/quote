4012d330c3a1216f4a86aeea5ac4033f4e8021e6
najmaA <najmaamin10@gmai.com>
2019-12-02 15:39:30 +0300

    content edited

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import {Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quote:Quote[] = [
-    new Quote (1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2020,3,14)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2020,3,14)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2020,3,14)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2020,3,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2020,3,14)),
+    new Quote (1, 'posted on', 'Programs must be written for people to read, and only incidentally for machines to execute. ',new Date(2012,3,14)),
+    new Quote(2,'posted on','I have to buy cookies for the parrot',new Date(2017,3,14)),
+    new Quote(3,'posted on','Diana has her birthday coming up soon',new Date(2016,3,14)),
+    new Quote(4,'posted on','Pupper likes expensive snacks',new Date(2018,3,14)),
+    new Quote(5,'posted on','Damn Math',new Date(2019,3,14)),
+    new Quote(6,'posted on','Cause I am an evil overlord',new Date(2013,3,14)),
   ];
   toggleDetails(index){
     this.quote[index].showDescription = !this.quote[index].showDescription;
