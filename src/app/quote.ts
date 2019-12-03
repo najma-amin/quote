0c031e09d9dfa7beebb4e07cb4e8a688e52f06d3
najmaA <najmaamin10@gmai.com>
2019-12-03 13:07:50 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,8 +0,0 @@
-export class Quote {
-
-  public showDescription: boolean;
-  constructor(public id: number,public name: string,public description: string,  public completeDate: Date ){
-    this.showDescription=false;
-  }
-
-  }
